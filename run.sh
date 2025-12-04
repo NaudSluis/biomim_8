f2ea1a68879c06fcb67bdaaf15b86f39f6857de3
NaudSluis <naud.sluis@gmail.com>
2025-12-04 15:14:33 +0100

    ino instead of io

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ cd /home/group8/biomim_8 || { echo "Failed to change directory"; exit 1; }
 echo "Running Pi code..."
 
 #compile arduino code
-arduino-cli compile --fqbn arduino:avr:uno arduino_code/wash_loop
+arduino-cli compile --fqbn arduino:avr:uno arduino_code/wash_loop.ino
 if [ $? -ne 0 ]; then
     echo "Arduino code compilation failed"
     exit 1
@@ -15,7 +15,7 @@ fi
 echo "Arduino code compiled successfully"
 
 # upload arduino code
-arduino-cli upload -p /dev/ttyUSB0 --fqbn arduino:avr:uno arduino_code/wash_loop
+arduino-cli upload -p /dev/ttyUSB0 --fqbn arduino:avr:uno arduino_code/wash_loop.ino
 if [ $? -ne 0 ]; then
     echo "Arduino code upload failed"
     exit 1
