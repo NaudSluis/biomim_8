dd134d0827cd25b345f0d07339b70e72d1816592
NaudSluis <naud.sluis@gmail.com>
2025-12-04 13:36:59 +0100

    added error logging to run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
-cd /home/group8/biomim_8
+cd /home/group8/biomim_8 || { echo "Failed to change directory"; exit 1; }
 
 echo "Running Pi code..."
+
+#compile arduino code
 arduino-cli compile --fqbn arduino:avr:uno arduino_code/wash_loop.ino
+if [ $? -ne 0 ]; then
+    echo "Arduino code compilation failed"
+    exit 1
+fi
+echo "Arduino code compiled successfully"
+
+# upload arduino code
 arduino-cli upload -p /dev/ttyUSB0 --fqbn arduino:avr:uno arduino_code/wash_loop.ino
-# Example: Python program
+if [ $? -ne 0 ]; then
+    echo "Arduino code upload failed"
+    exit 1
+fi
+echo "Arduino code uploaded successfully"
+
+#Run python script
 python3 arduino_control.py
+if [ $? -ne 0 ]; then
+    echo "Python script execution failed"
+    exit 1
+fi
+echo "Python script executed successfully"
+
 
