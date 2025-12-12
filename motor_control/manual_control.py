a9d933d7f5cd30b2ae9e7a711fffbe4b2da0764c
NaudSluis <naud.sluis@gmail.com>
2025-12-12 13:47:20 +0100

    added relative import

--- motor_control/manual_control.py
+++ motor_control/manual_control.py
@@ -3,7 +3,7 @@ import termios
 import tty
 import time
 import threading
-from DRV8825 import DRV8825
+from .DRV8825 import DRV8825
 
 Motor1 = DRV8825(dir_pin=13, step_pin=19, enable_pin=12, mode_pins=(16, 17, 20))
 Motor1.SetMicroStep('softward', 'fullstep')
