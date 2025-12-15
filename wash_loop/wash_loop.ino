d345e637e06fde2e21cc0922412916f0cb40d557
NaudSluis <naud.sluis@gmail.com>
2025-12-15 16:11:22 +0100

    fixed sketch errors

--- wash_loop/wash_loop.ino
+++ wash_loop/wash_loop.ino
@@ -2,38 +2,37 @@
 #include <Servo.h>
 #include <ArduinoJson.h>
 
-// Define the pins where the servos are connected
-
 const int servo1Pin = 9;
+
+// Servo
+Servo servo1;
+
+AF_DCMotor motor1(1);  // M1
+AF_DCMotor motor2(2);  // M2
 
 void setup() {
   Serial.begin(9600);
   Serial.println("System initialized");
 
+  servo1.attach(servo1Pin);
+  servo1.write(0);
 }
 
-// Pump motor forward for 'time' milliseconds
-void pump(int motor_int, int time) {
-
-  AF_DCMotor motor(motor_int);
-
+void pump(AF_DCMotor &motor, int time) {
   motor.setSpeed(255);
   motor.run(FORWARD);
   delay(time);
   motor.run(RELEASE);
 }
 
-void turn_servo(int angle){
-
-  servo.attach(servo1Pin);
-  servo.write(angle);
-  servo.write(0);
-  servo.detach();
-
+void turn_servo(int angle) {
+  angle = constrain(angle, 0, 180);
+  servo1.write(angle);
+  delay(500);
+  servo1.write(0);
 }
 
 void loop() {
-
   if (Serial.available()) {
     String command = Serial.readStringUntil('\n');
     command.trim();
@@ -43,13 +42,13 @@ void loop() {
 
     if (command == "pump_soap") {
       pump(motor1, 5000);
-    } 
+    }
     else if (command == "pump_water") {
       pump(motor2, 5000);
-    } 
+    }
     else if (command == "rotate") {
       turn_servo(180);
-    } 
+    }
     else {
       Serial.println("Unknown command");
     }
