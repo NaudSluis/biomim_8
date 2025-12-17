dde19fe8b30a06d3cdc583d3c6108e9805649891
NaudSluis <naud.sluis@gmail.com>
2025-12-17 10:38:32 +0100

    first try to control left-right with keyboard

--- motor_control/manual_control.py
+++ motor_control/manual_control.py
@@ -9,10 +9,18 @@ from .DRV8825 import DRV8825
 Motor1 = DRV8825(dir_pin=13, step_pin=19, enable_pin=12, mode_pins=(16, 17, 20))
 Motor1.SetMicroStep('softward', '1/32step')
 
+Motor2 = DRV8825(dir_pin=24, step_pin=18, enable_pin=4, mode_pins=(21, 22, 27))
+Motor2.SetMicroStep('softward', '1/32step')
+
 is_moving_forward = False      # single step
 is_moving_backward = False     # single step
 continuous_forward = False     # toggle
 continuous_backward = False    # toggle
+is_moving_left = False      # single step
+is_moving_right = False     # single step
+continuous_left = False     # toggle
+continuous_right = False    # toggle
+
 y_axis = 0
 x_axis = 0
 running = True
@@ -51,6 +59,8 @@ def get_key_nonblocking():
 def keyboard_listener():
     global is_moving_forward, is_moving_backward
     global continuous_forward, continuous_backward, running
+    global is_moving_left, is_moving_right
+    global continuous_left, continuous_right
     
     ser = initialize_connection()
 
@@ -59,10 +69,17 @@ def keyboard_listener():
     tty.setcbreak(fd)
 
     print("Controls:")
-    print("  W = one step forward")
-    print("  S = one step backward")
-    print("  E = toggle continuous forward")
-    print("  D = toggle continuous backward")
+    print("  W = toggle continuous up")
+    print("  A = toggle continuous left")
+    print("  S = toggle continuous down")
+    print("  D = toggle continuous right")
+    print("  Q = Pump soap")
+    print("  D = Pump water")
+    print("  R = Rotate Sponge")
+    print("  X = Step right")
+    print("  Z = Step left")
+    print("  Y = Step up")
+    print("  H = Step Down")
     print("  SPACE = stop")
     print("  ESC = quit")
 
@@ -76,26 +93,45 @@ def keyboard_listener():
             key = key.lower()
 
             if key == "w":
-                is_moving_forward = True
-
-            elif key == "s":
-                is_moving_backward = True
-
-            elif key == "e":
                 continuous_forward = not continuous_forward
                 if continuous_forward:
                     continuous_backward = False  # stop opposite mode
-
-            elif key == "d":
+                    continuous_left = False
+                    continuous_right = False
+                    
+            elif key == "a":
+                continuous_left = not continuous_left
+                if continuous_left:
+                    continuous_right = False  # stop opposite mode
+                    continuous_backward = False
+                    continuous_forward = False
+            elif key == "s":
                 continuous_backward = not continuous_backward
                 if continuous_backward:
                     continuous_forward = False  # stop opposite mode
+                    continuous_left = False
+                    continuous_right = False
+            elif key == "d":
+                continuous_right = not continuous_right
+                if continuous_right:
+                    continuous_left = False  # stop opposite mode
+                    continuous_backward = False
+                    continuous_forward = False
+            elif key == "e":
+                send_arduino_signal(ser, 'pump_water')
+            elif key == "q":
+                send_arduino_signal(ser, 'pump_soap')
             elif key == "r":
                 send_arduino_signal(ser, 'rotate')
             elif key == "z":
-                send_arduino_signal(ser, 'pump_soap')
+                is_moving_left = True
             elif key == "x":
-                send_arduino_signal(ser, 'pump_water')
+                is_moving_right = True
+            elif key == "y":
+                is_moving_forward = True
+            elif key == "h":
+                is_moving_backward = True
+
             elif key == " ":
                 continuous_forward = False
                 continuous_backward = False
@@ -116,9 +152,17 @@ def step_motor_forward():
 def step_motor_backward():
     Motor1.TurnStep(Dir='backward', steps=20, stepdelay=0.000001)
 
+def step_motor_right():
+    Motor2.TurnStep(Dir='forward', steps=20, stepdelay=0.000001)
+
+def step_motor_left():
+    Motor2.TurnStep(Dir='backward', steps=20, stepdelay=0.000001)
+
 def motor_control_loop():
     global running, is_moving_forward, is_moving_backward
     global continuous_forward, continuous_backward
+    global is_moving_left, is_moving_right
+    global continuous_left, continuous_right
     global y_axis, x_axis
 
     while running:
@@ -140,6 +184,22 @@ def motor_control_loop():
         elif continuous_backward:
             step_motor_backward()
             y_axis -= 1
+
+        elif is_moving_right:
+            step_motor_right()
+            x_axis += 1
+
+        elif is_moving_left:
+            step_motor_left()
+            x_axis -= 1
+
+        elif continuous_right:
+            step_motor_backward()
+            x_axis += 1
+
+        elif continuous_left:
+            step_motor_backward()
+            x_axis -= 1
 
 
         else:
