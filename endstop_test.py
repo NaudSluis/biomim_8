33950653c4aae66f1b3f50e95d553d052b325dbc
NaudSluis <naud.sluis@gmail.com>
2026-01-13 15:59:29 +0100

    fixed pin

--- endstop_test.py
+++ endstop_test.py
@@ -1,10 +1,13 @@
 from gpiozero import Button
+from signal import pause
 
 X_MIN_PIN = 11
 
-x_min = Button(X_MIN_PIN, pull_up=True) 
+x_min = Button(X_MIN_PIN, pull_up=True)
 
 def on_x_min():
     print("X min endstop hit")
-    
+
 x_min.when_pressed = on_x_min
+
+pause()  # keeps the program alive
