fc003cfdfca33125fd22a49ee9f172411af28199
NaudSluis <naud.sluis@gmail.com>
2026-01-13 17:53:19 +0100

    swithc DRV8825 back

--- motor_control/DRV8825.py
+++ motor_control/DRV8825.py
@@ -1,7 +1,7 @@
 """
-DRV8825 driver rewritten using gpiozero
+This is a package downloaded that is made for the driver
 """
-from gpiozero import DigitalOutputDevice
+import RPi.GPIO as GPIO
 import time
 
 MotorDir = [
@@ -10,72 +10,71 @@ MotorDir = [
 ]
 
 ControlMode = [
-    'hardware',
-    'software',
+    'hardward',
+    'softward',
 ]
 
 class DRV8825():
     def __init__(self, dir_pin, step_pin, enable_pin, mode_pins):
-        # GPIOZero devices
-        self.dir_pin = DigitalOutputDevice(dir_pin)
-        self.step_pin = DigitalOutputDevice(step_pin)
-        self.enable_pin = DigitalOutputDevice(enable_pin)
-        self.mode_pins = [DigitalOutputDevice(p) for p in mode_pins]
-
+        self.dir_pin = dir_pin
+        self.step_pin = step_pin        
+        self.enable_pin = enable_pin
+        self.mode_pins = mode_pins
+        
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup(self.dir_pin, GPIO.OUT)
+        GPIO.setup(self.step_pin, GPIO.OUT)
+        GPIO.setup(self.enable_pin, GPIO.OUT)
+        GPIO.setup(self.mode_pins, GPIO.OUT)
+        
     def digital_write(self, pin, value):
-        """Set a DigitalOutputDevice high or low"""
-        if isinstance(pin, list) or isinstance(pin, tuple):
-            # multiple pins at once
-            for p, v in zip(pin, value):
-                p.value = v
-        else:
-            pin.value = value
-
+        GPIO.output(pin, value)
+        
     def Stop(self):
-        """Disable the motor"""
-        self.enable_pin.off()
-
+        self.digital_write(self.enable_pin, 0)
+    
     def SetMicroStep(self, mode, stepformat):
         """
-        Set microstepping mode
-        (1) mode:
-            'hardware' : Use switches on the module
-            'software' : Use software to control microstep pins
-        (2) stepformat:
-            'fullstep', 'halfstep', '1/4step', '1/8step', '1/16step', '1/32step'
+        (1) mode
+            'hardward' :    Use the switch on the module to control the microstep
+            'software' :    Use software to control microstep pin levels
+                Need to put the All switch to 0
+        (2) stepformat
+            ('fullstep', 'halfstep', '1/4step', '1/8step', '1/16step', '1/32step')
         """
-        microstep = {
-            'fullstep':  (0, 0, 0),
-            'halfstep':  (1, 0, 0),
-            '1/4step':   (0, 1, 0),
-            '1/8step':   (1, 1, 0),
-            '1/16step':  (0, 0, 1),
-            '1/32step':  (1, 0, 1)
-        }
+        microstep = {'fullstep': (0, 0, 0),
+                     'halfstep': (1, 0, 0),
+                     '1/4step': (0, 1, 0),
+                     '1/8step': (1, 1, 0),
+                     '1/16step': (0, 0, 1),
+                     '1/32step': (1, 0, 1)}
 
-        print("Control mode:", mode)
-        if mode == ControlMode[1]:  # software
-            print("Setting microstep pins:", stepformat)
+        print ("Control mode:", mode)
+        if (mode == ControlMode[1]):
+            print ("set pins")
             self.digital_write(self.mode_pins, microstep[stepformat])
-
+        
     def TurnStep(self, Dir, steps, stepdelay=0.005):
-        """Turn motor a given number of steps"""
-        if Dir == MotorDir[0]:  # forward
-            self.enable_pin.on()
-            self.dir_pin.off()
-        elif Dir == MotorDir[1]:  # backward
-            self.enable_pin.on()
-            self.dir_pin.on()
+        if (Dir == MotorDir[0]):
+            # print ("forward")
+            self.digital_write(self.enable_pin, 1)
+            self.digital_write(self.dir_pin, 0)
+        elif (Dir == MotorDir[1]):
+            # print ("backward")
+            self.digital_write(self.enable_pin, 1)
+            self.digital_write(self.dir_pin, 1)
         else:
-            print("The dir must be: 'forward' or 'backward'")
-            self.Stop()
+            print ("the dir must be : 'forward' or 'backward'")
+            self.digital_write(self.enable_pin, 0)
             return
 
-        if steps == 0:
+        if (steps == 0):
             return
-
-        for _ in range(steps):
-            self.step_pin.on()
+            
+        # print ("turn step:",steps)
+        for i in range(steps):
+            self.digital_write(self.step_pin, True)
             time.sleep(stepdelay)
-            self.step_pin.off()
+            self.digital_write(self.step_pin, False)
             time.sleep(stepdelay)
