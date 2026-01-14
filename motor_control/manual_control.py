e8d75fe2bbb272fc933d55d4626d10e22f239139
NaudSluis <naud.sluis@gmail.com>
2026-01-14 11:25:28 +0100

    added logic for not moving while endstop is pressed

--- motor_control/manual_control.py
+++ motor_control/manual_control.py
@@ -13,34 +13,6 @@ from gpiozero import Button
 from gpiozero import Device
 from gpiozero.pins.rpigpio import RPiGPIOFactory
 
-DeviceFactory = RPiGPIOFactory()
-
-
-# Choose GPIOs for endstops
-Y_MIN_PIN = 6
-X_MIN_PIN = 5
-
- # switch pulls S to GND when hit
-# x_min = Button(X_MIN_PIN, pull_up=True)
-
-
-
-# def on_x_min_():
-
-
-
-# For pin layout, checkout the Waveshare stepper HAT wiki
-def initialize_motors():
-    """
-    Initializes motors with pin layout
-    """
-    Motor1 = DRV8825(dir_pin=13, step_pin=19, enable_pin=12, mode_pins=(16, 17, 20))
-    Motor1.SetMicroStep("softward", "1/32step")
-
-    Motor2 = DRV8825(dir_pin=24, step_pin=18, enable_pin=4, mode_pins=(21, 22, 27))
-    Motor2.SetMicroStep("softward", "1/32step")
-    return Motor1, Motor2
-
 is_moving_forward = False  # single step
 is_moving_backward = False  # single step
 continuous_forward = False  # toggle
@@ -54,7 +26,37 @@ y_axis = 0  # Used for calibration counter
 x_axis = 0  # Used for calibration counter
 running = True
 
-def on_y_min():
+# Ensures that variables work across threads
+y_min_pressed = threading.Event()
+x_min_pressed = threading.Event()
+
+DeviceFactory = RPiGPIOFactory()
+
+# Choose GPIOs for endstops
+Y_MIN_PIN = 6
+X_MIN_PIN = 5
+
+y_min = Button(Y_MIN_PIN, pull_up=False)
+x_min = Button(X_MIN_PIN, pull_up=True)
+
+
+def on_x_min_pressed():
+    global continuous_forward, continuous_backward
+    global continuous_left, continuous_right
+
+    x_min_pressed.set()
+
+    continuous_forward = False
+    continuous_backward = False
+    continuous_left = False
+    continuous_right = False
+    print("x min endstop hit, all continuous movements stopped.")
+
+def on_x_min_released():
+    x_min_pressed.clear()
+    print("x min endstop released.")
+
+def on_y_min_pressed():
     global continuous_forward, continuous_backward
     global continuous_left, continuous_right
 
@@ -63,6 +65,22 @@ def on_y_min():
     continuous_left = False
     continuous_right = False
     print("y min endstop hit, all continuous movements stopped.")
+
+def on_y_min_released():
+    y_min_pressed.clear()
+    print("y min endstop released.")
+
+# For pin layout, checkout the Waveshare stepper HAT wiki
+def initialize_motors():
+    """
+    Initializes motors with pin layout
+    """
+    Motor1 = DRV8825(dir_pin=13, step_pin=19, enable_pin=12, mode_pins=(16, 17, 20))
+    Motor1.SetMicroStep("softward", "1/32step")
+
+    Motor2 = DRV8825(dir_pin=24, step_pin=18, enable_pin=4, mode_pins=(21, 22, 27))
+    Motor2.SetMicroStep("softward", "1/32step")
+    return Motor1, Motor2
 
 
 
@@ -121,7 +139,7 @@ def keyboard_listener():
     global is_moving_left, is_moving_right
     global continuous_left, continuous_right
 
-    # ser = initialize_connection()
+    ser = initialize_connection()
 
     fd = sys.stdin.fileno()
     old_settings = termios.tcgetattr(fd)
@@ -132,8 +150,6 @@ def keyboard_listener():
     print("  A = toggle continuous left")
     print("  S = toggle continuous down")
     print("  D = toggle continuous right")
-    print("  E = Pump soap")
-    print("  Q = Pump water")
     print("  R = Rotate Sponge")
     print("  X = Step right")
     print("  Z = Step left")
@@ -177,12 +193,8 @@ def keyboard_listener():
                     continuous_left = False  # stop opposite mode
                     continuous_backward = False
                     continuous_forward = False
-            # elif key == "e":
-            #     send_arduino_signal(ser, "pump_soap")
-            # elif key == "q":
-            #     send_arduino_signal(ser, "pump_water")
-            # elif key == "r":
-            #     send_arduino_signal(ser, "rotate")
+            elif key == "r":
+                send_arduino_signal(ser, "rotate")
             elif key == "z":
                 is_moving_left = True
             elif key == "x":
@@ -204,14 +216,11 @@ def keyboard_listener():
 
     finally:
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        # ser.close()
+        ser.close()
 
 
 def step_motor_forward():
-    global Motor1, Motor2
-    # Increase step or decrease delay for higher speed until 
     Motor1.TurnStep(Dir="forward", steps=20, stepdelay=0.00000001)
-
 
 def step_motor_backward():
     Motor1.TurnStep(Dir="backward", steps=20, stepdelay=0.00000001)
@@ -233,45 +242,55 @@ def motor_control_loop():
     global y_axis, x_axis
 
     while running:
-
+        # -------- Y AXIS --------
         if is_moving_forward:
-            step_motor_forward()
-            y_axis += 1
+            if not y_min_pressed.is_set():
+                step_motor_forward()
+                y_axis += 1
             is_moving_forward = False
+
+        elif continuous_forward:
+            if not y_min_pressed.is_set():
+                step_motor_forward()
+                y_axis += 1
+            else:
+                time.sleep(0.01)
 
         elif is_moving_backward:
             step_motor_backward()
             y_axis -= 1
             is_moving_backward = False
 
-        elif continuous_forward:
-            step_motor_forward()
-            y_axis += 1
-
         elif continuous_backward:
             step_motor_backward()
             y_axis -= 1
+
+        # -------- X AXIS --------
+        elif is_moving_left:
+            if not x_min_pressed.is_set():
+                step_motor_left()
+                x_axis -= 1
+            is_moving_left = False
+
+        elif continuous_left:
+            if not x_min_pressed.is_set():
+                step_motor_left()
+                x_axis -= 1
+            else:
+                time.sleep(0.01)
 
         elif is_moving_right:
             step_motor_right()
             x_axis += 1
             is_moving_right = False
 
-        elif is_moving_left:
-            step_motor_left()
-            x_axis -= 1
-            is_moving_left = False
-
         elif continuous_right:
             step_motor_right()
             x_axis += 1
 
-        elif continuous_left:
-            step_motor_left()
-            x_axis -= 1
-
         else:
             time.sleep(0.01)
+
 
 
 def start_manual_control():
@@ -281,9 +300,12 @@ def start_manual_control():
     Motor1, Motor2 = initialize_motors()
 
     y_min = Button(6, pull_up=False) 
-    y_min.when_pressed = on_y_min
+    y_min.when_pressed = on_y_min_pressed
+    y_min.when_released = on_y_min_released
 
-    
+    x_min = Button(5, pull_up=True) 
+    x_min.when_pressed = on_x_min_pressed
+    x_min.when_released = on_x_min_released
 
     running = True
 
@@ -306,7 +328,7 @@ def start_manual_control():
     Motor2.step_pin.close()
     Motor2.enable_pin.close()
 
-    x_min.close()
+    y_min.close()
 
     print("Program exited.")
 
