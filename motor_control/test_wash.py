0f5429ff40fc9bdf0dd410f972c60e3fbf9d0ce1
NaudSluis <naud.sluis@gmail.com>
2026-01-22 12:22:19 +0100

    initialised all in demo

--- motor_control/test_wash.py
+++ motor_control/test_wash.py
@@ -1,11 +1,14 @@
 import json
 from motor_control import manual_control
-from motor_control.calibrate import move_to_home
+from motor_control.calibrate import move_to_home, reset_manual_state
 from motor_control.manual_control import initialize_motors, pump_one_forward, pump_two_forward, rotate_sponge, move_to_position
 from .DRV8825 import DRV8825
 import time
 import serial
 from datetime import datetime
+import threading
+from gpiozero import Button, Device
+from gpiozero.pins.rpigpio import RPiGPIOFactory
 
 
 def get_calibrated_postion(json_file: str):
@@ -41,8 +44,36 @@ def demo():
     """
     global Motor1, Motor2
 
+    # Initialize GPIO and endstops
+    Device.pin_factory = RPiGPIOFactory()
+    
+    y_min = Button(manual_control.Y_MIN_PIN, pull_up=True)
+    x_min = Button(manual_control.X_MIN_PIN, pull_up=True)
+    
+    y_min.when_pressed = manual_control.on_y_min_pressed
+    y_min.when_released = manual_control.on_y_min_released
+    
+    x_min.when_pressed = manual_control.on_x_min_pressed
+    x_min.when_released = manual_control.on_x_min_released
+
+    # Initialize motors
     Motor1, Motor2, pump1, pump2 = initialize_motors()
+    manual_control.Motor1 = Motor1
+    manual_control.Motor2 = Motor2
+    manual_control.pump1 = pump1
+    manual_control.pump2 = pump2
+    
     manual_control.running = True
+
+    # Start motor control loop thread
+    motor_thread = threading.Thread(
+        target=manual_control.motor_control_loop,
+        daemon=True
+    )
+    motor_thread.start()
+    
+    # Give thread time to start
+    time.sleep(0.1)
 
     start = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
@@ -90,8 +121,22 @@ def demo():
         return
     finally:
         manual_control.running = False
-        Motor1.disable()
-        Motor2.disable()
+        Motor1.Stop()
+        Motor2.Stop()
+        
+        # Clean up GPIO
+        for p in Motor1.mode_pins + Motor2.mode_pins:
+            p.close()
+        
+        Motor1.dir_pin.close()
+        Motor1.step_pin.close()
+        Motor1.enable_pin.close()
+        Motor2.dir_pin.close()
+        Motor2.step_pin.close()
+        Motor2.enable_pin.close()
+        
+        y_min.close()
+        x_min.close()
 
     end = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
